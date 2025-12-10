45cd73e1f6c031fbb67977a5d21144bdd8f92d55
Zeal.T <teszael14@gmail.com>
2025-12-11 01:58:13 +0300

    upadate sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,17 +1,7 @@
 module.exports = {
-    siteUrl: 'https://zealtesfaye.me',
-    generateRobotsTxt: true,
-    sitemapSize: 7000,
-    generateIndexSitemap: true, // Set to true to create an index sitemap file if needed
-    outDir: './public', // Ensures the sitemap is saved to the public folder
-    additionalPaths: async (config) => {
-        return [
-            { loc: '/about' },    // Add each route manually
-            { loc: '/contact' },
-            { loc: '/projects' },
-            { loc: '/skills' },
-            { loc: '/' },         // Root path
-            // Add more static or dynamic paths if needed
-        ];
-    },
+  siteUrl: "https://zealtesfaye.dev",
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  generateIndexSitemap: true, // Set to true to create an index sitemap file if needed
+  outDir: "./public", // Ensures the sitemap is saved to the public folder
 };
