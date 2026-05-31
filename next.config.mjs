4566d87de16be6e4c04d5eabe20782271e8cb093
Zeal.T <teszael14@gmail.com>
2026-05-31 18:42:23 +0300

    fix: pdf viewer browser compatibility

--- next.config.mjs
+++ next.config.mjs
@@ -1,26 +1,26 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    eslint: {
-        ignoreDuringBuilds: true, // Ignore ESLint during builds
-    },
-    images:{
-        domains: ['ltqzwwximfmgohiauqvj.supabase.co'],
-    },
-    typescript: {
-        ignoreBuildErrors: true, // This will allow the build to succeed despite type errors
-    },
-    async headers() {
-        return [
-            {
-                source: '/:path*.pdf',
-                headers: [
-                    { key: 'Content-Type', value: 'application/pdf' },
-                    { key: 'Content-Disposition', value: 'inline' },
-                    { key: 'X-Content-Type-Options', value: 'nosniff' },
-                ],
-            },
-        ];
-    },
+  eslint: {
+    ignoreDuringBuilds: true, // Ignore ESLint during builds
+  },
+  images: {
+    domains: ["ltqzwwximfmgohiauqvj.supabase.co"],
+  },
+  typescript: {
+    ignoreBuildErrors: true, // This will allow the build to succeed despite type errors
+  },
+  async headers() {
+    return [
+      {
+        source: "/:path*.pdf",
+        headers: [
+          { key: "Content-Type", value: "application/pdf" },
+          { key: "Content-Disposition", value: "inline" },
+          { key: "X-Content-Type-Options", value: "nosniff" },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
